0f517360a9ccb3bded9a23f9ff19747a815b3435
AnuBapatla <anusrichatala@gmail.com>
2025-12-29 22:39:30 -0500

    bootstrap added in index html and imported style files in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,36 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import "./style.css";
 
-createRoot(document.getElementById('root')).render(
-  <div>Hello</div>
+const root= createRoot(document.getElementById('root'));
+const pagestyle ={
+  textAlign:"center",
+  backgroundColor:"lighgray",
+  padding:"10px",
+}
+export function HomePage (){
+  return (
+    <div>
+      <Header/>
+      <p className="customHeading"> we are super exited to learn react</p>
+      <Footer/>
+      </div>
+    );
+}
+function Header(){
+  return(
+    <div>
+      <h1 style={pagestyle} className="text-success"> welcome to the react course</h1>
+    </div>
+  );
+}
+function Footer(){
+  return(
+    <div>
+      <p> made with doenet mystery</p>
+    </div>
+  )
+}
+root.render(
+  <HomePage/>
 )
-
 
