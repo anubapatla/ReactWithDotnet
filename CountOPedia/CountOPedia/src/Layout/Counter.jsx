4fae0b2c2bdfc29aa025b548ae10a151e4a9c9fe
AnuBapatla <anusrichatala@gmail.com>
2026-01-06 16:05:44 -0500

    random and reset button updated

--- CountOPedia/CountOPedia/src/Layout/Counter.jsx
+++ CountOPedia/CountOPedia/src/Layout/Counter.jsx
@@ -14,6 +14,18 @@ function Counter() {
         setCount((prev) => prev - 1);
         console.log(count);
     }
+    function handleRandomPlay() {
+        const playMode = Math.round(Math.random());
+        if (playMode == 0) {
+            handleIncrement();
+        } else {
+            handleDecrement();
+        }
+    }
+    function handleReset() {
+        setCount(0);
+    }
+
     return (
         <div className="container">
             <div className="row text-white container">
@@ -23,20 +35,19 @@ function Counter() {
                 <h3> Game Status :</h3>
                 <div className="col-6 col-md-3 offset-md-3">
                     <img src={logo} style={{ width: "100%", cursor: "pointer", border: "1px solid green" }}
+                        onClick={handleDecrement}
                         className="p-4 rounded"></img>
                 </div>
                 <div className="col-6 col-md-3 offset-md-3">
                     <img src={logo2} style={{ width: "100%", cursor: "pointer", border: "1px solid green" }}
+                        onClick={handleIncrement}
                         className="p-4 rounded"></img>
                 </div>
-                <button
-                    onClick={handleIncrement}
-                    className="btn btn-success m-2"
-                >
-                    +1
-                </button>
-                <button onClick={handleDecrement} className="btn btn-danger m-2">-1 </button>
-            </div >
+                <div className="col-12 col-md-4 offset-md-4">
+                    <button className="btn btn-success m-2 w-100" onClick={handleRandomPlay}>random play</button>
+                    <button className="btn btn-danger m-2 w-100" onClick={handleReset}>reset</button>
+                </div >
+            </div>
         </div>
     );
 }
