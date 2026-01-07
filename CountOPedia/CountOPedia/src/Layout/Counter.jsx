3a7091d8fcdc0c35d11c3c02ed18d3f32852a068
AnuBapatla <anusrichatala@gmail.com>
2026-01-06 22:59:14 -0500

    setted new set values

--- CountOPedia/CountOPedia/src/Layout/Counter.jsx
+++ CountOPedia/CountOPedia/src/Layout/Counter.jsx
@@ -3,17 +3,31 @@ import logo from "../Images/3d-icon-atomic-science-model-png.webp"
 import logo2 from "../Images/3d-atom-illustration-for-school-png.webp"
 function Counter() {
     let [count, setCount] = useState(0);
+    let [gameStatus, setGameStatus] = useState("")//you won or lost
 
     function handleIncrement() {
         setCount(function (prev) {
+            const newvalue = prev + 1;
+            if (newvalue >= 5) {
+                setGameStatus("you won");
+            }
+            else {
+                setGameStatus("");
+            }
             return prev + 1;
         });
-        console.log(count);
     }
     function handleDecrement() {
-        setCount((prev) => prev - 1);
-        console.log(count);
+        setCount(function (prev) {
+            const newValue = prev - 1;
+            if (newValue <= 5) {
+            } else {
+                setGameStatus("");
+            }
+            return newValue;
+        });
     }
+
     function handleRandomPlay() {
         const playMode = Math.round(Math.random());
         if (playMode == 0) {
@@ -25,14 +39,16 @@ function Counter() {
     function handleReset() {
         setCount(0);
     }
+    function handleLog() {
+        console.log(count);
+    }
 
     return (
         <div className="container">
             <div className="row text-white container">
                 <h1> Game Score:{count}</h1>
                 <p>you win at +5 points and lose at -5points</p>
-                <p>Last play:</p>
-                <h3> Game Status :</h3>
+                <h3> Game Status :{gameStatus}</h3>
                 <div className="col-6 col-md-3 offset-md-3">
                     <img src={logo} style={{ width: "100%", cursor: "pointer", border: "1px solid green" }}
                         onClick={handleDecrement}
@@ -46,6 +62,7 @@ function Counter() {
                 <div className="col-12 col-md-4 offset-md-4">
                     <button className="btn btn-success m-2 w-100" onClick={handleRandomPlay}>random play</button>
                     <button className="btn btn-danger m-2 w-100" onClick={handleReset}>reset</button>
+                    <button className="btn btn-warning m-2 w-100" onClick={handleLog}>log</button>
                 </div >
             </div>
         </div>
